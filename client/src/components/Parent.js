d41e884f3f57a385673eefc3251a9a47731395e5
Mwai <machariamwai@gmail.com>
2022-08-07 22:54:02 +0300

    Add User

--- client/src/components/Parent.js
+++ client/src/components/Parent.js
@@ -7,6 +7,7 @@ function Parent() {
   return (
     <div className="Parent text-center">
       <Navbar/>
+      {/* <h2>Welcome {user.username}</h2> */}
       <Posts/>
       <Footer/>
     </div>
